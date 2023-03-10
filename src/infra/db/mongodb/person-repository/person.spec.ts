71e26eda99c8085a64b662e3fd30dd59050e9f59
mikhael-abdallah <mikhael_abdallah@usp.br>
2023-03-10 01:11:45 -0300

    test: ensure PersonMongoRepository returns a person on success

---
+++ src/infra/db/mongodb/person-repository/person.spec.ts
@@ -0,0 +1,28 @@
+import { MongoHelper } from '../helpers/mongo-helper'
+import { PersonMongoRepository } from './person'
+
+describe('Person Mongo Repository', () => {
+  beforeAll(async () => {
+    const mongoUrl: string = process.env.MONGO_URL as string
+    expect(mongoUrl).toBeTruthy()
+    await MongoHelper.connect(mongoUrl)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  test('Should return an person on success', async () => {
+    const sut = new PersonMongoRepository()
+    const person = await sut.add({
+      name: 'any_name',
+      email: 'any_email',
+      birthDate: '2000-01-01'
+    })
+    expect(person).toBeTruthy()
+    expect(person.id).toBeTruthy()
+    expect(person.name).toBe('any_name')
+    expect(person.email).toBe('any_email')
+    expect(person.birthDate).toBe('2000-01-01')
+  })
+})
