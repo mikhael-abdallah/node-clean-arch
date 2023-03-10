13f0b8b89e210d1e54fb707dd5e4a2de2fb95745
mikhael-abdallah <mikhael_abdallah@usp.br>
2023-03-09 21:32:25 -0300

    feat: ensure DbAddAccount returns an account on success

--- src/data/usecases/add-person/db-add-person.ts
+++ src/data/usecases/add-person/db-add-person.ts
@@ -6,13 +6,8 @@ export class DbAddPerson implements AddPerson {
   }
 
   async add (personData: AddPersonModel): Promise<PersonModel> {
-    await this.addPersonRepository.add(personData)
+    const person = await this.addPersonRepository.add(personData)
 
-    return new Promise(resolve => {
-      resolve({
-        ...personData,
-        id: 1
-      })
-    })
+    return person
   }
 }
