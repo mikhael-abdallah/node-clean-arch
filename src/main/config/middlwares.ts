76cc6f0dfd7930f718b49921047aa9da981f9fa4
mikhael-abdallah <mikhael_abdallah@usp.br>
2023-03-10 02:44:22 -0300

    refactor: export all midlewares in a single file

--- src/main/config/middlwares.ts
+++ src/main/config/middlwares.ts
@@ -1,7 +1,5 @@
 import { Express } from 'express'
-import { bodyParserMiddleware } from '../middlewares/body-parser.middleware'
-import { corsMiddleware } from '../middlewares/cors.middleware'
-import { contentTypeMiddleware } from '../middlewares/content-type.middleware'
+import { bodyParserMiddleware, corsMiddleware, contentTypeMiddleware } from '../middlewares'
 export default (app: Express): void => {
   app.use(bodyParserMiddleware)
   app.use(corsMiddleware)
