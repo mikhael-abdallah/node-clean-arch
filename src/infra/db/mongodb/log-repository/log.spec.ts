65fd4bb96d9238a26d4e4ed5652a5f93167f0cf2
mikhael-abdallah <mikhael_abdallah@usp.br>
2023-03-10 20:35:38 -0300

    test: ensure LogMongoRepository creates an error log on success

---
+++ src/infra/db/mongodb/log-repository/log.spec.ts
@@ -0,0 +1,29 @@
+import { Collection } from 'mongodb'
+import { MongoHelper } from '../helpers/mongo-helper'
+import { LogMongoRepository } from './log'
+
+describe('Log Mongo Repository', () => {
+  let errorCollection: Collection
+
+  beforeAll(async () => {
+    const mongoUrl: string = process.env.MONGO_URL as string
+    expect(mongoUrl).toBeTruthy()
+    await MongoHelper.connect(mongoUrl)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    errorCollection = await MongoHelper.getCollection('error')
+    await errorCollection.deleteMany({})
+  })
+
+  test('Should create an error log on success', async () => {
+    const sut = new LogMongoRepository()
+    await sut.logError('any_error')
+    const count = await errorCollection.countDocuments()
+    expect(count).toBe(1)
+  })
+})
