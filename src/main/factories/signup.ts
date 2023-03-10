feaadb68f4618f2e5f0de29724aeb06f4a1389b5
mikhael-abdallah <mikhael_abdallah@usp.br>
2023-03-10 18:26:30 -0300

    feat: ensure log controller decorator calls controller with correct values

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -1,11 +1,14 @@
 import { DbAddPerson } from '../../data/usecases/add-person/db-add-person'
 import { PersonMongoRepository } from '../../infra/db/mongodb/person-repository/person'
 import { SignUpController } from '../../presentation/controllers/signup/signup.controller'
+import { Controller } from '../../presentation/protocols'
 import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
+import { LogControllerDecorator } from '../decorators/log'
 
-export const makeSignUpController = (): SignUpController => {
+export const makeSignUpController = (): Controller => {
   const emailValidatorAdapter = new EmailValidatorAdapter()
   const personMongoRepository = new PersonMongoRepository()
   const dbAddPerson = new DbAddPerson(personMongoRepository)
-  return new SignUpController(emailValidatorAdapter, dbAddPerson)
+  const signUpController = new SignUpController(emailValidatorAdapter, dbAddPerson)
+  return new LogControllerDecorator(signUpController)
 }
