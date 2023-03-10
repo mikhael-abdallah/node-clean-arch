4577b5dcc7483b9088a191cb4015ae9061579d8a
mikhael-abdallah <mikhael_abdallah@usp.br>
2023-03-10 16:46:59 -0300

    feat: ensure mongodb reconnect if its down

--- src/infra/db/mongodb/person-repository/person.ts
+++ src/infra/db/mongodb/person-repository/person.ts
@@ -5,7 +5,7 @@ import { MongoHelper } from '../helpers/mongo-helper'
 
 export class PersonMongoRepository implements AddPersonRepository {
   async add (personData: AddPersonModel): Promise<PersonModel> {
-    const personCollection = MongoHelper.getCollection('person')
+    const personCollection = await MongoHelper.getCollection('person')
     const result = await personCollection.insertOne(personData)
     const person = result.ops[0]
     return MongoHelper.map(person)
