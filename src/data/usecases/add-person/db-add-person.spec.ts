1f65cc532ef14e3129c93d0d69e164965eb6a68e
mikhael-abdallah <mikhael_abdallah@usp.br>
2023-03-09 21:15:30 -0300

    test: ensure DbAddAccount calls AddAccountRepository with correct values

--- src/data/usecases/add-person/db-add-person.spec.ts
+++ src/data/usecases/add-person/db-add-person.spec.ts
@@ -1,22 +1,48 @@
 import { DbAddPerson } from './db-add-person'
+import { AddPersonModel, AddPersonRepository, PersonModel } from './db-add-person-protocols'
+
+const makeAddPersonRepository = (): AddPersonRepository => {
+  class AddPersonRepositoryStub implements AddPersonRepository {
+    async add (personDate: AddPersonModel): Promise<PersonModel> {
+      const fakePerson = {
+        id: 1,
+        name: 'valid_name',
+        email: 'valid_email',
+        birthDate: '2000-01-01'
+      }
+      return new Promise(resolve => { resolve(fakePerson) })
+    }
+  }
+
+  return new AddPersonRepositoryStub()
+}
 
 interface SutTypes {
   sut: DbAddPerson
+  addPersonRepositoryStub: AddPersonRepository
 }
 
 const makeSut = (): SutTypes => {
-  const sut = new DbAddPerson()
-  return { sut }
+  const addPersonRepositoryStub = makeAddPersonRepository()
+  const sut = new DbAddPerson(addPersonRepositoryStub)
+  return { sut, addPersonRepositoryStub }
 }
 
 describe('DbAddPerson Usecase', () => {
-  test('', async () => {
-    const { sut } = makeSut()
+  test('Should call AddPersonRepository with correct values', async () => {
+    const { sut, addPersonRepositoryStub } = makeSut()
+    const addSpy = jest.spyOn(addPersonRepositoryStub, 'add')
+
     const personData = {
       name: 'valid_name',
       email: 'valid_email',
-      birthDate: 'valid_birthDate'
+      birthDate: '2000-01-01'
     }
     await sut.add(personData)
+    expect(addSpy).toHaveBeenCalledWith({
+      name: 'valid_name',
+      email: 'valid_email',
+      birthDate: '2000-01-01'
+    })
   })
 })
