25f831a4bbf60e348e4308cfbf7fc25f064241e6
mikhael-abdallah <mikhael_abdallah@usp.br>
2023-03-11 03:04:30 -0300

    refactor: change controller to make it uses prismas repositories

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,7 +1,13 @@
-import { MongoHelper } from '../infra/db/mongodb/helpers/mongo-helper'
+// import { MongoHelper } from '../infra/db/mongodb/helpers/mongo-helper'
+import prismaHelper from '../infra/db/prisma/helpers/prisma-helper'
 import env from './config/env'
 
-MongoHelper.connect(env.mongoUrl).then(async () => {
+// MongoHelper.connect(env.mongoUrl).then(async () => {
+//   const app = (await import('./config/app')).default
+//   app.listen(env.port, () => { console.log(`Server running at http://localhost:${env.port}`) })
+// }).catch(console.error)
+
+prismaHelper.connect().then(async () => {
   const app = (await import('./config/app')).default
   app.listen(env.port, () => { console.log(`Server running at http://localhost:${env.port}`) })
 }).catch(console.error)
