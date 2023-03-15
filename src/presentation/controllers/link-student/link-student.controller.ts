7a9076650150f6e05627af9cb1c78fe59839c5e0
mikhael-abdallah <mikhael_abdallah@usp.br>
2023-03-14 21:51:35 -0300

    feat: ensure LinkStudentController calls IntValidator with correct email

--- src/presentation/controllers/link-student/link-student.controller.ts
+++ src/presentation/controllers/link-student/link-student.controller.ts
@@ -1,12 +1,18 @@
 import { MissingParamError } from '../../errors'
 import { badRequest, ok } from '../../helpers/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '../../protocols'
+import { IntValidator } from '../../protocols/id-validator'
 
 export class LinkStudentController implements Controller {
+  constructor (private readonly intValidator: IntValidator) {
+  }
+
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     if (!httpRequest?.body?.id) {
       return badRequest(new MissingParamError('id'))
     }
+
+    this.intValidator.isValid(httpRequest.body.id)
 
     return ok({})
   }
---
+++ src/presentation/protocols/id-validator.ts
@@ -0,0 +1,3 @@
+export interface IntValidator {
+  isValid: (id: number) => boolean
+}
