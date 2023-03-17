af6c8fee412fa6f618a8f64c062976a93f05b022
mikhael-abdallah <mikhael_abdallah@usp.br>
2023-03-16 21:54:19 -0300

    refactor: move Validation interface to protocols folder

--- src/presentation/controllers/signup/signup-protocols.ts
+++ src/presentation/controllers/signup/signup-protocols.ts
@@ -1,4 +1,4 @@
 export * from '../../protocols/index'
 export * from '../../../domain/usecases/add-person'
 export * from '../../../domain/models/person'
-export * from '../../helpers/validators/validation'
+export * from '../../protocols/validation'
